4c49f08570ac2129615a2bf91533faa597fa92f5
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-12 11:19:32 +0000

    m

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //npm run dev to see this message in the terminal 
 console.log('hello')
 require('dotenv').config()
+//specify folder for static assets (index.html, js)
+const path = require('path')
 
 const express = require("express");
 const cors = require('cors')
@@ -8,11 +10,19 @@ const cors = require('cors')
 const server = express();
 server.use(express.json())
 server.use(cors())
+server.use(express.static(path.join(__dirname, 'client/build')))
+
 
 const port = process.env.PORT || 5000
 
 server.get('/api', (req, res) => {
     res.json({message: 'API works'})
+})
+
+//FALLBACK endpoint that will send back our index.html with the CRA
+server.get("*", (req, res) => {
+    //send back the index.html contained inside the client/build 
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 server.listen(port, () => {
