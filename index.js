1ca74a2f04b1ce604f4a8f8d2f9c2a3a2431a166
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-12 10:03:57 +0000

    m

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server = express();
 server.use(express.json())
 server.use(cors())
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 server.get('/api', (req, res) => {
     res.json({message: 'API works'})
