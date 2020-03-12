bb7016a2200513ac8bd6399f55d649b9bf11c3e6
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-12 09:48:59 +0000

    m

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 //npm run dev to see this message in the terminal 
 console.log('hello')
+
+const express = require("express");
+const cors = require('cors')
+
+const server = express();
+server.use(express.json())
+server.use(cors())
+
+const port = 5000
+
+server.get('/api', (req, res) => {
+    res.json({message: 'API works'})
+})
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  })
